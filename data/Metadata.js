7d81c4d90e9357162baebad13d72fe36ae6800b6
elcreativedeveloper <109305027+elcreativedeveloper@users.noreply.github.com>
2023-02-20 14:24:56 +0700

    Add files via upload

--- data/Metadata.js
+++ data/Metadata.js
@@ -1,8 +1,8 @@
 export const metadata = {
     title: "EL Creative Acadamy",
     description: "A blog created with Next.js and Tailwind.css",
-    url: "https://www.elcreativeacademy.com/",
-    urlOrigin: "https://www.elcreativeacademy.com/",
+    url: "https://www.dpdpanpringsewu.com/",
+    urlOrigin: "https://www.dpdpanpringsewu.com/",
 
     urlTwitter: "https://twitter.com/Twitter",
     imageFeatured: "http://localhost:3000/images/featured-image.jpg"
